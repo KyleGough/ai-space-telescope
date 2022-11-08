09b9a0ec6ae05cc39fe9facc28709c5dbd1cd1b3
Kyle Gough <kylegough98@gmail.com>
2022-11-08 17:37:28 +0000

    MVP with 3 images

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { Image } from './components';
+import { Image, Gallery } from './components';
 import img1 from './img/1667744916682.png';
+import img2 from './img/1667745366210.png';
+import img3 from './img/1667745366633.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className="container mx-auto px-4 md:px-8 lg:px-16">
+      <Gallery title="Space">
         <Image src={img1} caption="Caption 1" />
-      </header>
+        <Image src={img2} caption="Caption 1" />
+        <Image src={img3} caption="Caption 1" />
+      </Gallery>
     </div>
   );
 }
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 interface ImageProps {
-  src: string
-  caption: string
+  src: string;
+  caption: string;
 }
 
 export const Image: React.FC<ImageProps> = ({ src, caption }) => {
   return (
-    <div>
-      <img src={src} alt={caption} />
+    <div className="border-px border-white overflow-hidden">
+      <img
+        className="transition hover:scale-110 focus:scale-125 hover:brightness-110 focus:brightness-110 duration-200 cursor-pointer ease-out"
+        src={src}
+        alt={caption}
+      />
     </div>
   );
-}
+};
