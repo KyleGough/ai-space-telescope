7593c84b5fa52fb265894c2519146bb239ce26d6
Kyle Gough <kylegough98@gmail.com>
2022-11-10 23:50:56 +0000

    Improved dialog spacing and sizing responsiveness

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -14,10 +14,10 @@ export const Dialog: React.FC<DialogProps> = ({
   <div
     role="dialog"
     onClick={closeDialog}
-    className="fixed h-screen w-screen left-0 top-0 flex justify-center items-center bg-black/75 p-8 cursor-pointer"
+    className="fixed h-screen w-screen left-0 top-0 flex justify-center items-center bg-black/75 p-2 md:p-4 lg:p-8 cursor-pointer"
   >
     <div className="flex justify-center">
-      <div className="w-full rounded-sm overflow-hidden shadow-md m-4">
+      <div className="w-full rounded-sm overflow-hidden shadow-md m-2 md:m-4">
         <ResponsiveImage src={imageURL} alt={caption} />
       </div>
     </div>
