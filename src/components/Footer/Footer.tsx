40d3a5b2c7c27d6c7e5ff5b15eea917db9b6c719
Kyle Gough <kylegough98@gmail.com>
2022-11-10 19:23:41 +0000

    Defer sirv script, and add aria-label to footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,13 +6,19 @@ export const Footer: React.FC = () => (
     <div className="container mx-auto">
       <div className="flex items-center justify-center">
         <p className="mr-4">© Kyle Gough, 2022</p>
-        <a className="p-2" href="https://kylegough.co.uk/" target="_blank">
+        <a
+          className="p-2"
+          href="https://kylegough.co.uk/"
+          target="_blank"
+          aria-label="Personal Website"
+        >
           <HomeIcon className="duration-200 transition-colors fill-white hover:fill-stone-300 focus:fill-stone-300 w-6 h-6" />
         </a>
         <a
           className="p-2"
           href="https://github.com/KyleGough/ai-space-telescope"
           target="_blank"
+          aria-label="GitHub Profile"
         >
           <GitHubIcon className="duration-200 transition-colors fill-white hover:fill-stone-300 focus:fill-stone-300 w-5 h-5" />
         </a>
